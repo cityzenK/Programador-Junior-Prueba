9f8017f33e5bec7103f9af5bfcedd28ad11a0f66
cityzenK <eliezergoh@gmail.com>
2024-09-01 13:20:17 -0600

    upload file

---
+++ pruebaProgramador/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+bool isEven(int number)
+{
+    if (number % 2 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+int squared(int number)
+{
+    return (number * number);
+}
+
+Dictionary<char, int> fillDictionary(String word)
+{
+    word = word.ToLower().Replace(" ", "");
+    Console.WriteLine(word);
+    Dictionary<char, int> map = new Dictionary<char, int>();
+    foreach (char c in word)
+    {
+        if (!map.ContainsKey(c))
+        {
+            map.Add(c, 1);
+        }
+        else
+        {
+            map[c]++;
+        }
+    }
+    return map;
+}
+
+bool isAnagram(String word1, String word2)
+{
+    Dictionary<char, int> map = new Dictionary<char, int>();
+    Dictionary<char, int> map2 = new Dictionary<char, int>();
+
+    map = fillDictionary(word1);
+    map2 = fillDictionary(word2);
+    
+    return map.Count == map2.Count && !map.Except(map2).Any();
+}
+Console.WriteLine(isEven(2));
+Console.WriteLine(squared(2));
+Console.WriteLine(isAnagram("Fotolitografía  ", "Litofotografía"));
